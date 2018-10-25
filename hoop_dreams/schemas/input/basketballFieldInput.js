4ab1e92a170a6fb11e74d54ca24fed2cfdc0f8e3
unknown <bergros.gigja@gmail.com>
2018-10-25 19:02:01 +0000

    added input types

---
+++ hoop_dreams/schemas/input/basketballFieldInput.js
@@ -0,0 +1,8 @@
+module.exports = `
+    input BasketballFieldInput {
+        name: String!
+        capacity: int!
+        yearOfCreation: Moment!
+        status: BasketballFieldStatus!
+    }
+`;
---
+++ hoop_dreams/schemas/input/index.js
@@ -0,0 +1,11 @@
+const basketballFieldInput = require('./basketballFieldInput');
+const pickupGameInput = require('./PickupGameInput');
+const playerInput = require('./PlayerInput');
+const signupPlayerInput = require('./SignupPlayerInput');
+
+module.exports = `
+    ${basketballFieldInput}
+    ${pickupGameInput}
+    ${playerInput}
+    ${signupPlayerInput}
+`;
