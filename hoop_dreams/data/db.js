0b9bc1dbbbe0f09742254d8f6c429f3ebc007311
unknown <bergros.gigja@gmail.com>
2018-10-28 20:17:18 +0000

    added to resolve mmongoose odify error

--- hoop_dreams/data/db.js
+++ hoop_dreams/data/db.js
@@ -8,6 +8,8 @@ const connection = mongoose.createConnection('mongodb://user:u5erpassword@ds1417
     useNewUrlParser: true
 });
 
+mongoose.set('useFindAndModify', false);
+
 module.exports = {
     BasketballField: connection.model('BasketballField', BasketballFieldSchema),
     PickupGame: connection.model('PickupGame', pickupGameSchema),
